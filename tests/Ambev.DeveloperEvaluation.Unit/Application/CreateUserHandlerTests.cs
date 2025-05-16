12c5146f93546bcce91c5bc150401ad0fd439ed1
TalesSilva <tales.trab@gmail.com>
2025-05-16 04:13:07 -0700

    test: integration tests from scenarios of carts

--- tests/Ambev.DeveloperEvaluation.Unit/Application/CreateUserHandlerTests.cs
+++ tests/Ambev.DeveloperEvaluation.Unit/Application/CreateUserHandlerTests.cs
@@ -2,7 +2,6 @@ using Ambev.DeveloperEvaluation.Application.Users.CreateUser;
 using Ambev.DeveloperEvaluation.Common.Security;
 using Ambev.DeveloperEvaluation.Domain.Entities;
 using Ambev.DeveloperEvaluation.Domain.Repositories;
-using Ambev.DeveloperEvaluation.Unit.Domain;
 using AutoMapper;
 using FluentAssertions;
 using NSubstitute;
