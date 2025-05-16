108274fd0d867cee450891d07ca9c2816e47c1f9
TalesSilva <tales.trab@gmail.com>
2025-05-16 01:54:20 -0700

    feat: new command to cancel a cart

---
+++ src/Ambev.DeveloperEvaluation.Application/Carts/CancelCart/CancelCartResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Ambev.DeveloperEvaluation.Application.Carts.CancelCart
+{
+    /// <summary>
+    /// Response model for CancelCart operation.
+    /// </summary>
+    public class CancelCartResponse
+    {
+        /// <summary>
+        /// Indicates whether the cancellation was successful.
+        /// </summary>
+        public bool Success { get; set; }
+    }
+}
