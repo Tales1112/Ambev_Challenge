c1f5872842cbc0c73bdc62625bce7df47764b954
TalesSilva <tales.trab@gmail.com>
2025-05-15 22:47:58 -0700

    feat: create navigation between category and product to filter beign used and implement query in category repository

--- src/Ambev.DeveloperEvaluation.ORM/Repositories/CategoryRepository.cs
+++ src/Ambev.DeveloperEvaluation.ORM/Repositories/CategoryRepository.cs
@@ -2,38 +2,46 @@
 using Ambev.DeveloperEvaluation.Domain.Repositories;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace Ambev.DeveloperEvaluation.ORM.Repositories
 {
-    namespace Ambev.DeveloperEvaluation.ORM.Repositories
+    /// <summary>
+    /// Implementation of <see cref="ICategoryRepository"/> using Entity Framework Core.
+    /// </summary>
+    public class CategoryRepository : ICategoryRepository
     {
+        private readonly DefaultContext _context;
+
         /// <summary>
-        /// Implementation of <see cref="ICategoryRepository"/> using Entity Framework Core.
+        /// Initializes a new instance of <see cref="CategoryRepository"/>.
         /// </summary>
-        public class CategoryRepository : ICategoryRepository
+        /// <param name="context">The database context</param>
+        public CategoryRepository(DefaultContext context)
         {
-            private readonly DefaultContext _context;
+            _context = context;
+        }
 
-            /// <summary>
-            /// Initializes a new instance of <see cref="CategoryRepository"/>.
-            /// </summary>
-            /// <param name="context">The database context</param>
-            public CategoryRepository(DefaultContext context)
-            {
-                _context = context;
-            }
+        /// <inheritdoc/>
+        public void Create(Category category)
+        {
+            _context.Categories.Add(category);
+        }
 
-            /// <inheritdoc/>
-            public void Create(Category category)
-            {
-                _context.Categories.Add(category);
-            }
+        /// <inheritdoc/>
+        public async Task<Category?> GetByNameAsync(string name, CancellationToken cancellationToken = default)
+        {
+            return await _context.Categories
+                .FirstOrDefaultAsync(u => EF.Functions.ILike(u.Name, $"%{name}%"), cancellationToken);
+        }
 
-            /// <inheritdoc/>
-            public async Task<Category?> GetByNameAsync(string name, CancellationToken cancellationToken = default)
-            {
-                return await _context.Categories
-                    .FirstOrDefaultAsync(u => EF.Functions.ILike(u.Name, $"%{name}%"), cancellationToken);
-            }
+        /// <inheritdoc/>
+        public async Task<ICollection<string>> ListAllCategoriesBeingUsedAsync(CancellationToken cancellationToken)
+        {
+            return await _context.Categories
+                .Where(c => c.Products.Any())
+                .Select(c => c.Name)
+                .ToArrayAsync();
         }
     }
 }
+
