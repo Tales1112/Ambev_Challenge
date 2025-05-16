a57e3357e3aa51db13f04da28c7ca6bb75da49b5
TalesSilva <tales.trab@gmail.com>
2025-05-16 00:39:18 -0700

    feat: get cart details by id request

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Carts/GetCart/GetCartRequest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Carts.GetCart
+{
+    /// <summary>
+    /// Request model for getting a product by ID.
+    /// </summary>
+    public class GetCartRequest
+    {
+        /// <summary>
+        /// The unique identifier of the product to retrieve.
+        /// </summary>
+        [FromRoute(Name = "id")]
+        public Guid Id { get; set; }
+    }
+}
