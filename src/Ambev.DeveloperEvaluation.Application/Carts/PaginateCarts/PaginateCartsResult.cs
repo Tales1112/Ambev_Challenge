d86705940354343c50d3f7d4bc8885d973bbbf34
TalesSilva <tales.trab@gmail.com>
2025-05-16 00:24:28 -0700

    feat: create paginate list carts

---
+++ src/Ambev.DeveloperEvaluation.Application/Carts/PaginateCarts/PaginateCartsResult.cs
@@ -0,0 +1,11 @@
+﻿using Ambev.DeveloperEvaluation.Common.Repositories.Pagination;
+
+namespace Ambev.DeveloperEvaluation.Application.Carts.PaginateCarts
+{
+    /// <summary>
+    /// Response model for PaginateCarts operation.
+    /// </summary>
+    public class PaginateCartsResult : PaginationQueryResult<CartResult>
+    {
+    }
+}
