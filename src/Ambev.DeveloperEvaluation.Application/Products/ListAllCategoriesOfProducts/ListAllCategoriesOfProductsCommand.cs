e4495d07de9a9e9b04bc06d9f55b5fb91f82de4e
TalesSilva <tales.trab@gmail.com>
2025-05-15 22:50:02 -0700

    feat: create new command to list all categories

---
+++ src/Ambev.DeveloperEvaluation.Application/Products/ListAllCategoriesOfProducts/ListAllCategoriesOfProductsCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Products.ListAllCategoriesOfProducts
+{
+    /// <summary>
+    /// Command for retrieving all categories of product.
+    /// </summary>
+    public class ListAllCategoriesOfProductsCommand : IRequest<ListAllCategoriesOfProductsResult>
+    {
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Application/Products/ListAllCategoriesOfProducts/ListAllCategoriesOfProductsResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Ambev.DeveloperEvaluation.Application.Products.ListAllCategoriesOfProducts
+{
+    /// <summary>
+    /// Response model for ListAllCategoriesOfProducts operation.
+    /// </summary>
+    public class ListAllCategoriesOfProductsResult : List<string>
+    {
+    }
+}
